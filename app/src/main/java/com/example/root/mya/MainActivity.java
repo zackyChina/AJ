c9873cd46645adfff9c1c4bdd3c9ad27df09261d
zcx <zcx@foxmail.com>
2018-09-03 00:01:06 -0700

    2

---
+++ app/src/main/java/com/example/root/mya/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.root.mya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void buClick(View a){
+
+    }
+}
